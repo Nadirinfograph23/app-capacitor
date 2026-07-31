ee87e96f56e367161979e29879e2236a1bb1a126
Nadirinfograph23 <nadirfortest44@gmail.com>
2026-07-31 01:47:24 +0100

    ⚡ DZ Agent: add android/app/src/main/java/com/dzapp/app/MainActivity.java

---
+++ android/app/src/main/java/com/dzapp/app/MainActivity.java
@@ -0,0 +1,11 @@
+package com.dzapp.app;
+
+import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
